02f3924211214664ebfe79c7bb358027b7bf2330
Dr. Naila Aaijaz <125146655+nailajaz@users.noreply.github.com>
2026-07-19 12:46:30 -0400

    Simplify welcome section in app.py
    Removed subheader and updated welcome message.

--- app.py
+++ app.py
@@ -1,8 +1,6 @@
 import streamlit as st
 
-st.set_page_config(page_title="EntreReady AI", page_icon="🚀")
+st.set_page_config(page_title="EntreReady AI")
 
 st.title("EntreReady AI (ERAI)")
-st.subheader("An Explainable AI Decision Support Framework")
-
-st.write("Welcome to the first version of EntreReady AI.")
+st.write("Welcome!")
